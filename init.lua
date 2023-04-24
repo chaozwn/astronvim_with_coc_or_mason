2edfd3e1859f7f111b790157645e8b5d57c8923d
zhao_wei_nan <326747337@qq.com>
2023-04-24 23:46:37 +0800

    提高定时时间

--- init.lua
+++ init.lua
@@ -41,7 +41,7 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
       },
-      timeout_ms = 2000, -- default format timeout
+      timeout_ms = 10000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end

17917d0818eb00d407c35c7744f140d0d03d5d43
zhao_wei_nan <326747337@qq.com>
2023-05-05 13:02:48 +0800

    add python debugger

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -77,28 +77,14 @@ return {
     dependencies = { "mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter" },
     event = "LspAttach",
     opts = {
+      commented = true,
       enabled = true, -- enable this plugin (the default)
       enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-      highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-      highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-      show_stop_reason = true, -- show stop reason when stopped for exceptions
-      commented = false, -- prefix virtual text with comment string
-      only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
-      all_references = false, -- show virtual text on all all references of the variable (not only definitions)
-      --- A callback that determines how a variable is displayed or whether it should be omitted
-      --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
-      --- @param buf number
-      --- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
-      --- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
-      --- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
-      display_callback = function(variable, _buf, _stackframe, _node) return variable.name .. " = " .. variable.value end,
-
-      -- experimental features:
-      virt_text_pos = "eol", -- position of virtual text, see `:h nvim_buf_set_extmark()`
-      all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-      virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
-      virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
-      -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
     },
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    event = "LspAttach",
+    config = function() require("dap-python").setup("python", {}) end,
   },
 }

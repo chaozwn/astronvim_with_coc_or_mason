b661da6bb6e5707a1682fb36546d5ea47980f7f2
zhao_wei_nan <326747337@qq.com>
2023-05-13 11:45:09 +0800

    modify mini indent

--- highlights/init.lua
+++ highlights/init.lua
@@ -2,10 +2,9 @@ return function()
   local get_hlgroup = require("astronvim.utils").get_hlgroup
   local nontext = get_hlgroup "NonText"
   return {
-    CursorLineFold = { link = "CursorLineNr" }, -- highlight fold indicator as well as line number
+    CursorLineFold = { link = "CursorLineNr" },                    -- highlight fold indicator as well as line number
     GitSignsCurrentLineBlame = { fg = nontext.fg, italic = true }, -- italicize git blame virtual text
-    HighlightURL = { underline = true }, -- always underline URLs
-    OctoEditable = { fg = "NONE", bg = "NONE" }, -- use treesitter for octo.nvim highlighting
+    HighlightURL = { underline = true },                           -- always underline URLs
     -- leap
     LeapMatch = { bg = "#ff007c", fg = "#c8d3f5", bold = true },
     LeapLabelPrimary = { fg = "#ff007c", bold = true },
--- plugins/user.lua
+++ plugins/user.lua
@@ -35,14 +35,14 @@ return {
       require("neoscroll").setup {
         -- All these keys will be mapped to their corresponding default scrolling animation
         mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
-        hide_cursor = true, -- Hide cursor while scrolling
-        stop_eof = true, -- Stop at <EOF> when scrolling downwards
-        respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        hide_cursor = true,          -- Hide cursor while scrolling
+        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
         cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-        easing_function = nil, -- Default easing function
-        pre_hook = nil, -- Function to run before the scrolling animation starts
-        post_hook = nil, -- Function to run after the scrolling animation ends
-        performance_mode = false, -- Disable "Performance Mode" on all buffers.
+        easing_function = nil,       -- Default easing function
+        pre_hook = nil,              -- Function to run before the scrolling animation starts
+        post_hook = nil,             -- Function to run after the scrolling animation ends
+        performance_mode = false,    -- Disable "Performance Mode" on all buffers.
       }
       local t = {}
       -- Syntax: t[keys] = {function, {function arguments}}
@@ -69,8 +69,8 @@ return {
     event = "User AstroFile",
     opts = {
       -- symbol = "▏",
-      symbol = "│",
-      options = { try_as_border = true },
+      symbol = "╎",
+      options = { try_as_border = false },
     },
     init = function()
       vim.api.nvim_create_autocmd("FileType", {

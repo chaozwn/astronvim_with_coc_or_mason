b6f20e3c2ce1f19ad80d7295e7244a6b6f057f6c
zhao_wei_nan <326747337@qq.com>
2023-05-16 18:07:40 +0800

    add vue controller

--- lsp/config/volar.lua
+++ lsp/config/volar.lua
@@ -2,6 +2,15 @@ return {
   filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
   settings = {
     vue = {
+      autoInsert = {
+        dotValue = true,
+      },
+      updateImportsOnFileMove = true,
+      server = {
+        petiteVue = {
+          supportHtmlFile = true,
+        },
+      },
       inlayHints = {
         inlineHandlerLeading = true,
         missingProp = true,

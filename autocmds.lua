332b6a43e980362fb08b7888e098a5bb1b8f7c10
zhao_wei_nan <326747337@qq.com>
2023-05-29 19:59:19 +0800

    add jupyter

--- autocmds.lua
+++ autocmds.lua
@@ -46,10 +46,9 @@ local lsp_type = require("user.config.lsp_type").lsp_type
 if lsp_type == 'coc' then
   vim.api.nvim_create_augroup("CocGroup", {})
 
-  vim.cmd("command! -nargs=? Fold :call CocAction('fold', <f-args>)")
-  -- NOTE: 这边一定要用这个，coc的menu有渲染问题
-  vim.cmd("hi! link CocPum Pmenu")
-  vim.cmd("hi! link CocMenuSel PmenuSel")
+  -- vim.cmd("command! -nargs=? Fold :call CocAction('fold', <f-args>)")
+  -- vim.cmd("hi! link CocPum Pmenu")
+  -- vim.cmd("hi! link CocMenuSel PmenuSel")
   vim.cmd("hi CocFloating ctermbg=235 guibg=#13354A")
   -- vim.cmd("hi CocMenuSel ctermbg=237 guibg=#13354A")
   vim.cmd("highlight CocHighlightText guibg=#545c7e")
--- plugins/hop.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- 'phaazon/hop.nvim',
-  -- branch = 'v2', -- optional but strongly recommended
-  -- event = "BufRead",
-  -- opts = {}
-}

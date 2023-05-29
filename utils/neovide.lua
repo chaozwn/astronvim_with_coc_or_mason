12db21807963ec5bd6a9f50e9d35974636c6c8fa
zhao_wei_nan <326747337@qq.com>
2023-05-29 19:29:24 +0800

    fix neovide font

--- utils/neovide.lua
+++ utils/neovide.lua
@@ -7,7 +7,7 @@ function M.init()
 end
 
 function M.config()
-  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h15:i"
+  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h16"
   -- 没有空闲
   vim.g.neovide_no_idle = true
   -- 退出需要确认

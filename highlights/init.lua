01eb38f9bc9b4bce87f29a28251882ec12b3da86
zhao_wei_nan <326747337@qq.com>
2023-06-01 22:21:38 +0800

    add telescope nachad scheme

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,7 +1,26 @@
 return function()
   local get_hlgroup = require("astronvim.utils").get_hlgroup
   local nontext = get_hlgroup "NonText"
+  -- get highlights from highlight groups
+  local normal = get_hlgroup "Normal"
+  local fg, bg = normal.fg, normal.bg
+  local bg_alt = get_hlgroup("Visual").bg
+  local green = get_hlgroup("String").fg
+  local red = get_hlgroup("Error").fg
+
   return {
+    TelescopeBorder = { fg = bg_alt, bg = bg },
+    TelescopeNormal = { bg = bg },
+    TelescopePreviewBorder = { fg = bg, bg = bg },
+    TelescopePreviewNormal = { bg = bg },
+    TelescopePreviewTitle = { fg = bg, bg = green },
+    TelescopePromptBorder = { fg = bg_alt, bg = bg_alt },
+    TelescopePromptNormal = { fg = fg, bg = bg_alt },
+    TelescopePromptPrefix = { fg = red, bg = bg_alt },
+    TelescopePromptTitle = { fg = bg, bg = red },
+    TelescopeResultsBorder = { fg = bg, bg = bg },
+    TelescopeResultsNormal = { bg = bg },
+    TelescopeResultsTitle = { fg = bg, bg = bg },
     CursorLineFold = { link = "CursorLineNr" },                    -- highlight fold indicator as well as line number
     GitSignsCurrentLineBlame = { fg = nontext.fg, italic = true }, -- italicize git blame virtual text
     HighlightURL = { underline = true },                           -- always underline URLs

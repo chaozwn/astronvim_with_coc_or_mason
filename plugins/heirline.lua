238c111a509018587fcb6080312a235202754766
zhao_wei_nan <326747337@qq.com>
2023-06-09 10:59:33 +0800

    add file encoding

--- plugins/heirline.lua
+++ plugins/heirline.lua
@@ -2,27 +2,71 @@ return {
   "rebelot/heirline.nvim",
   opts = function(_, opts)
     local status = require "astronvim.utils.status"
+    local utils = require "astronvim.utils"
+    local extend_tbl = utils.extend_tbl
 
-    function StatusDiagnostic()
-      ---@diagnostic disable-next-line: undefined-field
-      local info = vim.b.coc_diagnostic_info or {}
-      if next(info) == nil then return "" end
-      local msgs = {}
-      if info.error and info.error > 0 then table.insert(msgs, "E" .. info.error) end
-      if info.warning and info.warning > 0 then table.insert(msgs, "W" .. info.warning) end
-      return table.concat(msgs, " ") .. " " .. (vim.g.coc_status or "")
+    function StatusDiagnostic(o)
+      return function(self)
+        ---@diagnostic disable-next-line: undefined-field
+        local info = vim.b.coc_diagnostic_info or {}
+        if next(info) == nil then return "" end
+        local msgs = {}
+        if info.error and info.error > 0 then table.insert(msgs, "E" .. info.error) end
+        if info.warning and info.warning > 0 then table.insert(msgs, "W" .. info.warning) end
+        return status.utils.stylize(table.concat(msgs, " ") .. " " .. (vim.g.coc_status or ""), o)
+      end
     end
 
     local coc_lsp = {
-      provider = function() return StatusDiagnostic() end,
+      provider = StatusDiagnostic(),
       update = { "User", pattern = { "CocStatusChange", "CocDiagnosticChange" } },
       init = status.init.update_events { 'User AstroFile' },
       callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end),
       hl = { fg = "green", bold = false },
+      padding = { left = 1, right = 1 },
     }
 
-    if vim.g.lsp_type == "coc" then
-      opts.statusline[9] = coc_lsp
+    local file_encoding_component = function(o)
+      o = extend_tbl({
+        hl = status.hl.get_attributes "git_branch",
+        provider = status.provider.file_encoding({ padding = { left = 1 } }),
+      })
+      return status.component.builder(status.utils.setup_providers(o, {}))
     end
+
+    if vim.g.lsp_type == "coc" then
+      opts.statusline = {
+        hl = { fg = "fg", bg = "bg" },
+        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+        status.component.git_branch(),
+        status.component.file_info { filetype = {}, filename = false, file_modified = false },
+        status.component.git_diff(),
+        status.component.diagnostics(),
+        status.component.fill(),
+        status.component.cmd_info(),
+        status.component.fill(),
+        coc_lsp,
+        file_encoding_component(),
+        status.component.treesitter(),
+        status.component.nav(),
+      }
+    else
+      opts.statusline = {
+        hl = { fg = "fg", bg = "bg" },
+        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+        status.component.git_branch(),
+        status.component.file_info { filetype = {}, filename = false, file_modified = false },
+        status.component.git_diff(),
+        status.component.diagnostics(),
+        status.component.fill(),
+        status.component.cmd_info(),
+        status.component.fill(),
+        status.component.lsp(),
+        status.component.treesitter(),
+        status.component.file_encoding(),
+        status.component.nav(),
+      }
+    end
+    return opts
   end,
 }

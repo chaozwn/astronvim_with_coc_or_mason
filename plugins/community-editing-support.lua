c2c75ee4083f9b1e8adee4e0bca687dacb3e4b8a
zhao_wei_nan <326747337@qq.com>
2023-07-16 00:44:32 +0800

    feat: add rainbow delimiters

--- plugins/community-editing-support.lua
+++ plugins/community-editing-support.lua
@@ -3,6 +3,7 @@ return {
   { import = "astrocommunity.editing-support.auto-save-nvim" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
   { import = "astrocommunity.editing-support.zen-mode-nvim" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   {
     "folke/zen-mode.nvim",
     opts = {
@@ -15,24 +16,6 @@ return {
           list = true, -- show whitespace characters
         },
       },
-      on_open = function() -- disable diagnostics and indent blankline
-        vim.g.diagnostics_mode_old = vim.g.diagnostics_mode
-        vim.b.miniindentscope_disable_old = vim.b.miniindentscope_disable
-        vim.g.winbar_old = vim.wo.winbar
-        vim.g.diagnostics_mode = 0
-        vim.diagnostic.config(require("astronvim.utils.lsp").diagnostics[0])
-        -- vim.g.indent_blankline_enabled_old = vim.g.indent_blankline_enabled
-        -- vim.g.indent_blankline_enabled = false
-        vim.wo.winbar = nil
-        vim.b.miniindentscope_disable = true
-      end,
-      on_close = function() -- restore diagnostics and indent blankline
-        vim.g.diagnostics_mode = vim.g.diagnostics_mode_old
-        vim.diagnostic.config(require("astronvim.utils.lsp").diagnostics[vim.g.diagnostics_mode])
-        vim.wo.winbar = vim.g.winbar_old
-        vim.b.miniindentscope_disable = vim.b.miniindentscope_disable_old
-        -- vim.g.indent_blankline_enabled = vim.g.indent_blankline_enabled_old
-      end,
     },
   },
 }

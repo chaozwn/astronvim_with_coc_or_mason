91322dce6a848d88098b42144b40c9d8a1782a24
赵伟楠 <zhaown@zhaoweinandeMacBook-Pro.local>
2023-07-23 01:29:33 +0800

    feat: add executable check in pack and remove nocie in telescope

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -27,6 +27,6 @@ return {
     require "plugins.configs.telescope"(...)
     local telescope = require "telescope"
     telescope.load_extension "refactoring"
-    telescope.load_extension "noice"
+    if not vim.g.neovide then telescope.load_extension "noice" end
   end,
 }

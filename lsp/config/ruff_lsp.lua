cb48e0ec6768006a45e81b2db745a36b980f47f8
Yulduck Sung <yulduck@gmail.com>
2023-10-12 11:55:37 +0900

    merged

--- lsp/config/ruff_lsp.lua
+++ lsp/config/ruff_lsp.lua
@@ -1,25 +1,25 @@
-local util = require "lspconfig/util"
-
-return {
-  root_dir = function(fname)
-    local root_files = {
-      "pyproject.toml",
-      "setup.py",
-      "setup.cfg",
-      "requirements.txt",
-      "Pipfile",
-      "pyrightconfig.json",
-      "main.py",
-    }
-    return util.find_git_ancestor(fname) or util.root_pattern(unpack(root_files))(fname)
-  end,
-  init_options = {
-    settings = {
-      -- Any extra CLI arguments for `ruff` go here.
-      args = { "--config=$HOME/.config/ruff/ruff.toml" },
-      organizeImports = true,
-      fixAll = true,
-    },
-  },
-  settings = {},
-}
+-- local util = require "lspconfig/util"
+--
+-- return {
+--   root_dir = function(fname)
+--     local root_files = {
+--       "pyproject.toml",
+--       "setup.py",
+--       "setup.cfg",
+--       "requirements.txt",
+--       "Pipfile",
+--       "pyrightconfig.json",
+--       "main.py",
+--     }
+--     return util.find_git_ancestor(fname) or util.root_pattern(unpack(root_files))(fname)
+--   end,
+--   init_options = {
+--     settings = {
+--       -- Any extra CLI arguments for `ruff` go here.
+--       args = { "--config=$HOME/.config/ruff/ruff.toml" },
+--       organizeImports = true,
+--       fixAll = true,
+--     },
+--   },
+--   settings = {},
+-- }

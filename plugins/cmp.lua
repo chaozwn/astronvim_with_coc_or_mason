84dde62f81e8d35939b9cf31fe952bf57ef6d5a6
zhao_wei_nan <326747337@qq.com>
2023-10-31 14:48:07 +0800

    Revert "feat: fix pack deprecated."
    This reverts commit 25ec405f3f2716ce2fd99c0defac3426ad32f89d.

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -12,7 +12,7 @@ return {
     local luasnip = require "luasnip"
 
     local function has_words_before()
-      local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
     end
 

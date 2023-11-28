87ad1d1dfbe9b23e5df89eee5db229126ad16491
Loqoqo <support@loqoqo.com>
2023-11-28 10:27:50 +0900

    many changes

--- plugins/community-debugging.lua
+++ plugins/community-debugging.lua
@@ -1,14 +1,14 @@
 return {
-  { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
-  {
-    "Weissle/persistent-breakpoints.nvim",
-    keys = function() return {} end,
-  },
-  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
-  {
-    "theHamsta/nvim-dap-virtual-text",
-    opts = {
-      virt_text_pos = "eol",
-    },
-  },
+  -- { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
+  -- {
+  --   "Weissle/persistent-breakpoints.nvim",
+  --   keys = function() return {} end,
+  -- },
+  -- { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+  -- {
+  --   "theHamsta/nvim-dap-virtual-text",
+  --   opts = {
+  --     virt_text_pos = "eol",
+  --   },
+  -- },
 }
--- plugins/community-pack.lua
+++ plugins/community-pack.lua
@@ -4,25 +4,25 @@ return {
   vim.fn.executable "npm" == 1 and { import = "astrocommunity.pack.tailwindcss" } or {},
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.thrift" },
-  (vim.fn.executable "python" == 1 or vim.fn.executable "python3" == 1) and {
-    import = "astrocommunity.pack.python-ruff",
-  } or {},
-  vim.fn.executable "npm" == 1 and { import = "astrocommunity.pack.docker" } or {},
-  vim.fn.executable "go" == 1 and { import = "astrocommunity.pack.go" } or {},
+  -- (vim.fn.executable "python" == 1 or vim.fn.executable "python3" == 1) and {
+  --   import = "astrocommunity.pack.python-ruff",
+  -- } or {},
+  -- vim.fn.executable "npm" == 1 and { import = "astrocommunity.pack.docker" } or {},
+  -- vim.fn.executable "go" == 1 and { import = "astrocommunity.pack.go" } or {},
   vim.fn.executable "npm" == 1 and { import = "astrocommunity.pack.yaml" } or {},
   vim.fn.executable "npm" == 1 and { import = "astrocommunity.pack.toml" } or {},
   vim.fn.executable "npm" == 1 and { import = "astrocommunity.pack.json" } or {},
   vim.fn.executable "npm" == 1 and { import = "astrocommunity.pack.html-css" } or {},
-  { import = "astrocommunity.pack.lua" },
-  vim.fn.executable "rustc" == 1 and { import = "astrocommunity.pack.rust" },
-  vim.fn.executable "protoc" == 1 and { import = "astrocommunity.pack.proto" } or {},
-  {
-    "linux-cultist/venv-selector.nvim",
-    ft = "python",
-    opts = {
-      anaconda_base_path = "~/miniconda3",
-      anaconda_envs_path = "~/miniconda3/envs",
-    },
-    keys = function() return {} end,
-  },
+  -- { import = "astrocommunity.pack.lua" },
+  -- vim.fn.executable "rustc" == 1 and { import = "astrocommunity.pack.rust" },
+  -- vim.fn.executable "protoc" == 1 and { import = "astrocommunity.pack.proto" } or {},
+  -- {
+  --   "linux-cultist/venv-selector.nvim",
+  --   ft = "python",
+  --   opts = {
+  --     anaconda_base_path = "~/miniconda3",
+  --     anaconda_envs_path = "~/miniconda3/envs",
+  --   },
+  --   keys = function() return {} end,
+  -- },
 }

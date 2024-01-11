2e50fa5669f35105ce65cc26169807fa30f52f4f
zhao_wei_nan <326747337@qq.com>
2024-01-12 01:24:56 +0800

    feat: remove unless

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -65,22 +65,6 @@ return {
             once = true,
           },
         },
-        auto_update_neotree_after_lazygit_close = {
-          {
-            event = "TermClose",
-            desc = "Refresh Neo-Tree when closing lazygit",
-            group = augroup("neotree_refresh", { clear = true }),
-            callback = function()
-              local manager_avail, manager = pcall(require, "neo-tree.sources.manager")
-              if manager_avail then
-                for _, source in ipairs { "filesystem", "git_status", "document_symbols" } do
-                  local module = "neo-tree.sources." .. source
-                  if package.loaded[module] then manager.refresh(require(module).name) end
-                end
-              end
-            end,
-          },
-        },
       },
       mappings = require("mappings").mappings(opts.mappings),
     })

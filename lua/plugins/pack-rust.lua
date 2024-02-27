1fe3074e9066c5cf3e5aed00641caae76f22c392
jayce.zhao <326747337@qq.com>
2024-02-27 16:22:04 +0800

    feat: update

--- lua/plugins/pack-rust.lua
+++ lua/plugins/pack-rust.lua
@@ -1,6 +1,3 @@
--- if you want to support rust, remove this line
-if true then return {} end
-
 local utils = require "astrocore"
 local function preview_stack_trace()
   local current_line = vim.api.nvim_get_current_line()

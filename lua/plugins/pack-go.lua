2074a9aaea9a8818cd7fa177ca018dde6cdb733c
zhao_wei_nan <326747337@qq.com>
2024-03-02 17:30:25 +0800

    feat: update

--- lua/plugins/pack-go.lua
+++ lua/plugins/pack-go.lua
@@ -52,7 +52,7 @@ return {
                 rangeVariableTypes = true,
               },
               analyses = {
-                fieldalignment = true,
+                fieldalignment = false,
                 nilness = true,
                 unusedparams = true,
                 unusedwrite = true,

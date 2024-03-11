acc0b9995a90291efd97d662fff82e9e63979d2f
jayce.zhao <326747337@qq.com>
2024-03-11 15:57:48 +0800

    feat: update

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,18 +1,28 @@
 return {
-  "AstroNvim/astroui",
-  ---@type AstroUIOpts
-  opts = {
-    colorscheme = "catppuccin-mocha",
-    highlights = {
-      init = function()
-        local get_hlgroup = require("astroui").get_hlgroup
-        return {
-          CursorLineFold = { link = "CursorLineNr" }, -- highlight fold indicator as well as line number
-          GitSignsCurrentLineBlame = { fg = get_hlgroup("NonText").fg, italic = true }, -- italicize git blame virtual text
-          HighlightURL = { underline = true }, -- always underline URLs
-          OctoEditable = { fg = "NONE", bg = "NONE" }, -- use treesitter for octo.nvim highlighting
-        }
-      end,
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = false,
+    },
+  },
+  {
+    "AstroNvim/astroui",
+    ---@type AstroUIOpts
+    opts = {
+      colorscheme = "solarized-osaka",
+      highlights = {
+        init = function()
+          local get_hlgroup = require("astroui").get_hlgroup
+          return {
+            CursorLineFold = { link = "CursorLineNr" }, -- highlight fold indicator as well as line number
+            GitSignsCurrentLineBlame = { fg = get_hlgroup("NonText").fg, italic = true }, -- italicize git blame virtual text
+            HighlightURL = { underline = true }, -- always underline URLs
+            OctoEditable = { fg = "NONE", bg = "NONE" }, -- use treesitter for octo.nvim highlighting
+          }
+        end,
+      },
     },
   },
 }

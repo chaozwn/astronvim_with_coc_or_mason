572b4ec55c7ccd3eb52a80983af5e6f68fa067ae
jayce.zhao <326747337@qq.com>
2024-03-13 23:46:58 +0800

    feat: update

--- lua/plugins/pack-go.lua
+++ lua/plugins/pack-go.lua
@@ -161,27 +161,27 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     opts = {
-      disable_defaults = true,
-      -- lsp_inlay_hints = {
-      --   enable = false,
-      -- },
-      -- diagnostic = { -- set diagnostic to false to disable vim.diagnostic setup
-      --   hdlr = true, -- hook lsp diag handler and send diag to quickfix
-      --   underline = true,
-      --   virtual_text = {
-      --     spacing = 5,
-      --     severity_limit = "ERROR",
-      --     severity = {
-      --       min = vim.diagnostic.severity.ERROR,
-      --     },
-      --   },
-      --   signs = {
-      --     severity = {
-      --       min = vim.diagnostic.severity.ERROR,
-      --     },
-      --   },
-      --   update_in_insert = false,
-      -- },
+      -- disable_defaults = true,
+      lsp_inlay_hints = {
+        enable = false,
+      },
+      diagnostic = { -- set diagnostic to false to disable vim.diagnostic setup
+        hdlr = true, -- hook lsp diag handler and send diag to quickfix
+        underline = true,
+        virtual_text = {
+          spacing = 5,
+          severity_limit = "ERROR",
+          severity = {
+            min = vim.diagnostic.severity.ERROR,
+          },
+        },
+        signs = {
+          severity = {
+            min = vim.diagnostic.severity.ERROR,
+          },
+        },
+        update_in_insert = false,
+      },
     },
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },

1fc749c85b39dfb4bb7841cac9b7ec5f5b805293
jayce.zhao <326747337@qq.com>
2024-03-14 17:28:03 +0800

    feat: update because none-ls.nvim merge my pr.

--- lua/plugins/pack-sql.lua
+++ lua/plugins/pack-sql.lua
@@ -37,15 +37,8 @@ return {
           generator_opts = buf_diagnostics_buildins._opts,
         })
         local buf_formatting_buildins = null_ls.builtins.formatting.sqlfluff
-        -- sqlfluff fix --disable-progress-bar -n -
-        buf_formatting_buildins._opts.args = {
-          "fix",
-          "--disable-progress-bar",
-          "-n",
-          "--config",
-          vim.fn.stdpath "config" .. "/.sqlfluff",
-          "-",
-        }
+        table.insert(buf_formatting_buildins._opts.args, "--config")
+        table.insert(buf_formatting_buildins._opts.args, vim.fn.stdpath "config" .. "/.sqlfluff")
         null_ls.register(null_ls.builtins.formatting.sqlfluff.with {
           generator_opts = buf_formatting_buildins._opts,
         })

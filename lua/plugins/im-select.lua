56c6c524eb103b022adb1a6487ad04c3e0a8c70b
jayce.zhao <326747337@qq.com>
2024-03-20 00:04:01 +0800

    feat: update

--- lua/plugins/im-select.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "chaozwn/im-select.nvim",
-  lazy = false,
-  opts = {
-    default_main_select = "im.rime.inputmethod.Squirrel.Hans",
-    set_previous_events = { "InsertEnter", "FocusLost" },
-  },
-}

3003277863213dbbd7a22efabbb9fb737a2aa61b
jayce.zhao <326747337@qq.com>
2024-03-21 19:22:08 +0800

    feat: update

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -2,7 +2,7 @@ return {
   "chaozwn/auto-save.nvim",
   event = { "User AstroFile", "InsertEnter" },
   opts = {
-    debounce_delay = 5000,
+    debounce_delay = 3000,
     print_enabled = false,
     callbacks = {
       before_saving = function()

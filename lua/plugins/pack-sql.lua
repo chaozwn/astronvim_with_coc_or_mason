1ac2642323bf2704b4cde229d1edd7792bb0f422
jayce.zhao <326747337@qq.com>
2024-03-26 00:16:45 +0800

    feat: update

--- lua/plugins/pack-sql.lua
+++ lua/plugins/pack-sql.lua
@@ -21,6 +21,13 @@ return {
     end,
   },
   {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "sqls" })
+    end,
+  },
+  {
     "jay-babu/mason-null-ls.nvim",
     optional = true,
     opts = function(_, opts)

cf817fc68c1bdf32b2b5f1af2dc8c03930ea02cd
jayce.zhao <326747337@qq.com>
2024-05-19 01:08:55 +0800

    feat: update auto-save

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -2,8 +2,23 @@ return {
   "chaozwn/auto-save.nvim",
   event = { "User AstroFile", "InsertEnter" },
   opts = {
-    debounce_delay = 3000,
+    debounce_delay = 5000,
     print_enabled = false,
+    trigger_events = { "TextChanged" },
+    condition = function(buf)
+      local fn = vim.fn
+      local utils = require "auto-save.utils.data"
+
+      if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+        -- check weather not in normal mode
+        if fn.mode() ~= "n" then
+          return false
+        else
+          return true -- met condition(s), can save
+        end
+      end
+      return false -- can't save
+    end,
     callbacks = {
       before_saving = function()
         -- save global autoformat status

95734969c152ba695ce61314260c9144cea18797
jayce.zhao <326747337@qq.com>
2024-05-20 09:38:16 +0800

    feat: update search

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -43,7 +43,7 @@ return {
           },
         },
         presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
+          bottom_search = false, -- use a classic bottom cmdline for search
           command_palette = false, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
           inc_rename = false, -- enables an input dialog for inc-rename.nvim

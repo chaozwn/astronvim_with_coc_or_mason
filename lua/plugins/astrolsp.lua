d0c62fa735aa2f6d69eada88c1a5ab366272d30c
jayce.zhao <326747337@qq.com>
2024-05-20 10:11:27 +0800

    feat: update

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,3 +1,4 @@
+---@type LazySpec
 return {
   "AstroNvim/astrolsp",
   ---@type AstroLSPOpts
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,4 @@
+---@type LazySpec
 return {
   { "max397574/better-escape.nvim", enabled = false },
   {

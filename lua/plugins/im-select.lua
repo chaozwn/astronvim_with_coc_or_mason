b9340d5fd55cb291e446d84b7d3ade347ec751b3
zhao_wei_nan <326747337@qq.com>
2024-06-15 17:05:22 +0800

    feat: update input method

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -2,7 +2,7 @@ return {
   "chaozwn/im-select.nvim",
   lazy = false,
   opts = {
-    default_main_select = "im.rime.inputmethod.Squirrel.Hans",
+    default_main_select = "com.sogou.inputmethod.sogou.pinyin",
     set_previous_events = { "InsertEnter", "FocusLost" },
   },
 }

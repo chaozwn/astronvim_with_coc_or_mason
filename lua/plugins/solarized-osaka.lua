39d352ad8127c70e070011c4696f92f14d7a53b4
jayce.zhao <326747337@qq.com>
2024-06-16 13:47:05 +0800

    feat: update theme

---
+++ lua/plugins/solarized-osaka.lua
@@ -0,0 +1,10 @@
+return {
+  "craftzdog/solarized-osaka.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = function(_, opts)
+    return require("astrocore").extend_tbl(opts, {
+      transparent = true,
+    })
+  end,
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -157,7 +157,7 @@ return {
           -- get highlights from highlight groups
           local normal = get_hlgroup "Normal"
           local fg, bg = normal.fg, normal.bg
-          local bg_alt = get_hlgroup("Visual").bg
+          local bg_alt = get_hlgroup("WinBar").bg
           local green = get_hlgroup("String").fg
           local red = get_hlgroup("Error").fg
           -- return a table of highlights for telescope based on
@@ -165,14 +165,14 @@ return {
           return {
             TelescopeBorder = { fg = bg_alt, bg = bg },
             TelescopeNormal = { bg = bg },
-            TelescopePreviewBorder = { fg = bg, bg = bg },
+            TelescopePreviewBorder = { fg = bg_alt, bg = bg },
             TelescopePreviewNormal = { bg = bg },
             TelescopePreviewTitle = { fg = bg, bg = green },
             TelescopePromptBorder = { fg = bg_alt, bg = bg_alt },
             TelescopePromptNormal = { fg = fg, bg = bg_alt },
             TelescopePromptPrefix = { fg = red, bg = bg_alt },
             TelescopePromptTitle = { fg = bg, bg = red },
-            TelescopeResultsBorder = { fg = bg, bg = bg },
+            TelescopeResultsBorder = { fg = bg_alt, bg = bg },
             TelescopeResultsNormal = { bg = bg },
             TelescopeResultsTitle = { fg = bg, bg = bg },
           }

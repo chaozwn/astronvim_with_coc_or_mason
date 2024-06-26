bc2a00e68db6ab246f2409b2ea4130f6fd3c841d
jayce.zhao <326747337@qq.com>
2024-06-26 17:36:57 +0800

    feat: update

---
+++ lua/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,24 @@
+-- TODO:https://github.com/AstroNvim/AstroNvim/issues/2593
+return {
+  { "NvChad/nvim-colorizer.lua", enabled = false },
+  {
+    "brenoprata10/nvim-highlight-colors",
+    event = "User AstroFile",
+    cmd = "HighlightColors",
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          local maps = opts.mappings
+          maps.n["<Leader>uz"] = { function() vim.cmd.HighlightColors "Toggle" end, desc = "Toggle color highlight" }
+        end,
+      },
+    },
+    opts = {
+      enabled_named_colors = false,
+      render = "virtual",
+      virtual_symbol_position = "inline",
+      enable_tailwind = true,
+    },
+  },
+}

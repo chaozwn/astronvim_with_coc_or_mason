71d46c96aa363a27d6d01427524a27df09283bfc
jayce.zhao <326747337@qq.com>
2024-07-03 00:07:02 +0800

    feat: add F2 use nvcheet

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -4,5 +4,17 @@ return {
   { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
   { import = "astrocommunity.lsp.nvim-lsp-file-operations" },
   { import = "astrocommunity.recipes.auto-session-restore" },
+  { import = "astrocommunity.keybinding.nvcheatsheet-nvim" },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<F1>"] = false,
+          ["<F2>"] = { function() require("nvcheatsheet").toggle() end, desc = "Cheatsheet" },
+        },
+      },
+    },
+  },
   -- { import = "astrocommunity.workflow.hardtime-nvim" },
 }

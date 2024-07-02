7dfc008119f7dcbca8bd7fddc9f0fdbea334460c
jayce.zhao <326747337@qq.com>
2024-07-02 13:57:47 +0800

    feat: update nvim-spectre

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -25,13 +25,7 @@ return {
     opts = function()
       return {
         mapping = {
-          send_to_qf = { map = "q" },
-          replace_cmd = { map = "c" },
-          show_option_menu = { map = "o" },
-          run_current_replace = { map = "C" },
-          run_replace = { map = "R" },
-          change_view_mode = { map = "v" },
-          resume_last_search = { map = "l" },
+          send_to_qf = { map = "<Leader>f" },
         },
       }
     end,

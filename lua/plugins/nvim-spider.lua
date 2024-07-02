54ad0ddf734a64687079faec8e8af0888c4de9f6
jayce.zhao <326747337@qq.com>
2024-07-02 12:08:40 +0800

    feat: add plugin spectre & spider

---
+++ lua/plugins/nvim-spider.lua
@@ -0,0 +1,36 @@
+local key_maps = {
+  ["w"] = {
+    "<Cmd>lua require('spider').motion('w')<CR>",
+    desc = "Next word",
+  },
+
+  ["e"] = {
+    "<Cmd>lua require('spider').motion('e')<CR>",
+    desc = "Next end of word",
+  },
+  ["b"] = {
+    "<Cmd>lua require('spider').motion('b')<CR>",
+    desc = "Previous word",
+  },
+  ["ge"] = {
+    "<Cmd>lua require('spider').motion('ge')<CR>",
+    desc = "Previous end of word",
+  },
+}
+
+return {
+  "chrisgrieser/nvim-spider",
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = key_maps,
+          x = key_maps,
+          o = key_maps,
+        },
+      },
+    },
+  },
+  opts = {},
+}
--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@ brew install gdu
 brew install bottom
 brew install protobuf
 brew install trash
+brew install gnu-sed
 
 pip install notebook nbclassic jupyter-console
 pip install git+https://github.com/will8211/unimatrix.git

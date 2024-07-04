d75553a1a1f965c383b9c54298286467fdf8fe33
jayce.zhao <326747337@qq.com>
2024-07-04 15:37:47 +0800

    feat: update barbecue logic, cancel auto attach

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -7,8 +7,33 @@ return {
   dependencies = {
     "SmiteshP/nvim-navic",
     "nvim-tree/nvim-web-devicons", -- optional dependency
+    {
+      "AstroNvim/astrolsp",
+      ---@type AstroLSPOpts
+      opts = {
+        autocmds = {
+          auto_attach_navic = {
+            {
+              event = "LspAttach",
+              desc = "Auto attach navic when lsp start",
+              callback = function(a)
+                local navic = require "nvim-navic"
+                local client = vim.lsp.get_client_by_id(a.data.client_id)
+                local exclude_clients = { "volar" }
+                if client then
+                  if client.server_capabilities["documentSymbolProvider"] then
+                    if not exclude_clients[client.name] then navic.attach(client, a.buf) end
+                  end
+                end
+              end,
+            },
+          },
+        },
+      },
+    },
   },
   opts = {
+    attach_navic = false,
     show_modified = true,
     exclude_filetypes = { "netrw", "toggleterm" },
   },

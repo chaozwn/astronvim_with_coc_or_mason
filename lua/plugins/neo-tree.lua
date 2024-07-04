2561de20412e7841b129804eaec722251cbfed58
jayce.zhao <326747337@qq.com>
2024-07-04 13:31:13 +0800

    feat: when neo-tree delete file to trash, close mapping buffer.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,3 +1,12 @@
+local function get_buffer_by_name(buf_name)
+  local buffers = vim.api.nvim_list_bufs()
+  for _, buf in ipairs(buffers) do
+    local name = vim.api.nvim_buf_get_name(buf)
+    if name:match(buf_name) then return buf end
+  end
+  return nil
+end
+
 local function trash(state)
   local inputs = require "neo-tree.ui.inputs"
   local cmds = require "neo-tree.sources.manager"
@@ -11,6 +20,8 @@ local function trash(state)
   inputs.confirm(msg, function(confirmed)
     if not confirmed then return end
     vim.api.nvim_command("silent !trash -F " .. node.path)
+    local buf_id = get_buffer_by_name(node.path)
+    if buf_id and buf_id ~= 0 then require("astrocore.buffer").close(buf_id, true) end
     cmds.refresh(state)
   end)
 end
@@ -28,6 +39,8 @@ local function trash_visual(state, selected_nodes)
     if not confirmed then return end
     for _, path in ipairs(paths_to_trash) do
       vim.api.nvim_command("silent !trash -F " .. path)
+      local buf_id = get_buffer_by_name(path)
+      if buf_id and buf_id ~= 0 then require("astrocore.buffer").close(buf_id, true) end
     end
     cmds.refresh(state)
   end)

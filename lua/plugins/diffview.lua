9f1bf0d3aab139121fe29362f61a7fc2afedb821
jayce.zhao <326747337@qq.com>
2024-07-05 11:56:23 +0800

    feat: add diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,4 +1,6 @@
 local prefix_diff_view = "<Leader>g"
+
+---@type LazySpec
 return {
   {
     "AstroNvim/astrocore",
@@ -37,6 +39,44 @@ return {
         },
         hooks = { diff_buf_read = function(bufnr) vim.b[bufnr].view_activated = false end },
       })
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "z3ht/telescope-diffviewer.nvim",
+    },
+    opts = function(_, opts)
+      local telescope = require "telescope"
+      require("astrocore").extend_tbl(opts, {
+        pickers = {
+          git_commits = {
+            mappings = {
+              n = { ["<C-r>d"] = telescope.extensions.diffviewer.diff },
+              i = { ["<C-r>d"] = telescope.extensions.diffviewer.diff },
+            },
+          },
+          git_bcommits = {
+            mappings = {
+              n = { ["<C-r>d"] = telescope.extensions.diffviewer.diff },
+              i = { ["<C-r>d"] = telescope.extensions.diffviewer.diff },
+            },
+          },
+          git_branches = {
+            mappings = {
+              n = { ["<C-r>d"] = telescope.extensions.diffviewer.diff },
+              i = { ["<C-r>d"] = telescope.extensions.diffviewer.diff },
+            },
+          },
+          git_stash = {
+            mappings = {
+              n = { ["<C-r>d"] = telescope.extensions.diffviewer.diff },
+              i = { ["<C-r>d"] = telescope.extensions.diffviewer.diff },
+            },
+          },
+        },
+      })
+      telescope.load_extension "diffviewer"
     end,
   },
   {

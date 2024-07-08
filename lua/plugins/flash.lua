937aeda357b878a5db79728abdf32b5c0e267cd1
jayce.zhao <326747337@qq.com>
2024-07-08 12:54:17 +0800

    feat: update

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -12,6 +12,6 @@ return {
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }

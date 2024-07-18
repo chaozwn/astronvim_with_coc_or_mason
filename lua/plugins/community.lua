822bc7002383f8c8a9b1831c991629a405456ddb
jayce.zhao <326747337@qq.com>
2024-07-18 12:13:29 +0800

    feat: support neovide

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -1,9 +1,39 @@
+local system = vim.loop.os_uname().sysname
+
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono Nerd Font:h16"
+  vim.g.neovide_no_idle = true
+  vim.g.neovide_confirm_quit = true
+  vim.g.neovide_fullscreen = false
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+
+  -- local alpha = function() return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8)) end
+  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+  vim.g.neovide_transparency = 0.9
+  vim.g.transparency = 0.9
+  -- vim.g.neovide_transparency = "#24283b"
+
+  if system == "Darwin" then
+    vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
+
+    vim.api.nvim_set_keymap("", "<D-c>", '"+y', { noremap = true, silent = true })
+    -- Allow clipboard copy paste in neovim
+    vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  end
+end
+
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
   { import = "astrocommunity.lsp.nvim-lsp-file-operations" },
   { import = "astrocommunity.recipes.auto-session-restore" },
+  { import = "astrocommunity.recipes.neovide" },
   { import = "astrocommunity.keybinding.nvcheatsheet-nvim" },
   {
     "AstroNvim/astrocore",
--- lua/plugins/solarized-osaka.lua
+++ lua/plugins/solarized-osaka.lua
@@ -1,14 +1,19 @@
-return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function(_, opts)
-    return require("astrocore").extend_tbl(opts, {
+local function init_opts()
+  if vim.g.neovide then
+    return {}
+  else
+    return {
       transparent = true,
       styles = {
         sidebars = "transparent",
         floats = "transparent",
       },
-    })
-  end,
+    }
+  end
+end
+return {
+  "craftzdog/solarized-osaka.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = function(_, opts) return require("astrocore").extend_tbl(opts, init_opts()) end,
 }

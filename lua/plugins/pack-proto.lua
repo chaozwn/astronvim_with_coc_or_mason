9309275d77bc975d679aa2ad614e316acb04b6e8
jayce.zhao <326747337@qq.com>
2024-07-23 18:40:21 +0800

    feat: update

--- lua/plugins/pack-proto.lua
+++ lua/plugins/pack-proto.lua
@@ -70,13 +70,6 @@ return {
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "bufls" })
-    end,
-  },
-  {
     "jay-babu/mason-null-ls.nvim",
     optional = true,
     opts = function(_, opts)

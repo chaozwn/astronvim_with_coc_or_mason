9dfd09101244fa41bcca76d2ebdfb31dd9217139
jayce.zhao <326747337@qq.com>
2024-08-11 01:10:40 +0800

    feat: remove whichkey fold

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,14 +1,30 @@
 ---@type LazySpec
 return {
-  "karb94/neoscroll.nvim",
-  event = "VeryLazy",
-  opts = { mappings = {
-    "<C-b>",
-    "<C-f>",
-    "<C-y>",
-    "<C-e>",
-    "zt",
-    "zz",
-    "zb",
-  } },
+  {
+    "AstroNvim/astrocore",
+    ---@param opts AstroCoreOpts
+    opts = function(_, opts)
+      if not opts.mappings then opts.mappings = require("astrocore").empty_map_table() end
+      local maps = opts.mappings
+      if maps then
+        maps.n["zz"] = { function() require("neoscroll").zz { half_win_duration = 250 } end, desc = "Center screen" }
+        maps.x["zz"] = maps.n["zz"]
+        maps.n["zt"] = { function() require("neoscroll").zt { half_win_duration = 250 } end, desc = "Top screen" }
+        maps.x["zt"] = maps.n["zt"]
+        maps.n["zb"] = { function() require("neoscroll").zb { half_win_duration = 250 } end, desc = "Bottom screen" }
+        maps.x["zb"] = maps.n["zb"]
+      end
+      opts.mappings = maps
+    end,
+  },
+  {
+    "karb94/neoscroll.nvim",
+    event = "VeryLazy",
+    opts = { mappings = {
+      "<C-b>",
+      "<C-f>",
+      "<C-y>",
+      "<C-e>",
+    } },
+  },
 }
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    plugins = {
+      spelling = {
+        enabled = false,
+      },
+      presets = {
+        z = false,
+      },
+    },
+  },
+}

71bb4e3caaf7959dfaef2881474adb2e47d6a444
jayce.zhao <326747337@qq.com>
2024-09-01 10:10:31 +0800

    feat: update render-markdown

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,5 +1,2 @@
 ---@type LazySpec
-return { "folke/tokyonight.nvim", opts = {
-  -- NOTE: https://github.com/folke/tokyonight.nvim/pull/620
-  plugins = { markdown = true },
-} }
+return { "folke/tokyonight.nvim", opts = {} }

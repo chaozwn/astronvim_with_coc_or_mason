67d0db3985a9a4a911fb32b849d1582bb01ec15a
jayce.zhao <326747337@qq.com>
2024-09-05 23:16:20 +0800

    feat: update

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
   { import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "astrotheme", "habamax" } },
+  install = { colorscheme = { "tokyonight-moon" } },
   ui = { backdrop = 100 },
   performance = {
     rtp = {

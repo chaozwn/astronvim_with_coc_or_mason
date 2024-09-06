2b14c225b568efc074001c8167b76bd37ef5f0e0
jayce.zhao <326747337@qq.com>
2024-09-06 10:29:32 +0800

    feat: update

--- lua/plugins/windows.lua
+++ lua/plugins/windows.lua
@@ -3,63 +3,22 @@
 local prefix = "<Leader>w"
 
 return {
-  {
-    "anuvyklack/windows.nvim",
-    lazy = false,
-    dependencies = {
-      "anuvyklack/middleclass",
-      "anuvyklack/animation.nvim",
-      {
-        "AstroNvim/astrocore",
-        opts = {
-          options = {
-            opt = {
-              winwidth = 10,
-              winminwidth = 10,
-              equalalways = false,
-            },
-          },
-          mappings = {
-            n = {
-              [prefix] = { desc = "󱂬 Window" },
-              [prefix .. "c"] = { "<C-w>c", desc = "Close current screen" },
-              [prefix .. "o"] = { "<C-w>o", desc = "Close other screen" },
-              [prefix .. "m"] = {
-                function() vim.cmd [[WindowsMaximize]] end,
-                desc = "Maximize Window",
-              },
-              [prefix .. "|"] = {
-                function() vim.cmd [[WindowsMaximizeHorizontally]] end,
-                desc = "Maximize Window Horizontally",
-              },
-              [prefix .. "\\"] = {
-                function() vim.cmd [[WindowsMaximizeVertically]] end,
-                desc = "Maximize Window Vertically",
-              },
-              [prefix .. "e"] = {
-                function() vim.cmd [[WindowsEqualize]] end,
-                desc = "Equalize Window",
-              },
-            },
-          },
-        },
+  "AstroNvim/astrocore",
+  opts = {
+    options = {
+      opt = {
+        winwidth = 10,
+        winminwidth = 10,
+        equalalways = false,
       },
     },
-    opts = {
-      ignore = {
-        -- :echo "Buffer Name: " . expand('%') . ", Buffer Type: " . &buftype
-        buftype = { "quickfix", "nofile", "terminal" },
-        filetype = { "NvimTree", "neo-tree", "undotree", "gundo" },
+    mappings = {
+      n = {
+        [prefix] = { desc = "󱂬 Window" },
+        [prefix .. "c"] = { "<C-w>c", desc = "Close current screen" },
+        [prefix .. "o"] = { "<C-w>o", desc = "Close other screen" },
+        [prefix .. "e"] = { "<C-w>=", desc = "Equals All Window" },
       },
-    },
-    cmd = {
-      "WindowsMaximize",
-      "WindowsMaximizeVertically",
-      "WindowsMaximizeHorizontally",
-      "WindowsEqualize",
-      "WindowsEnableAutowidth",
-      "WindowsDisableAutowidth",
-      "WindowsToggleAutowidth",
     },
   },
 }

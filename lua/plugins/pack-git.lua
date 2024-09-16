d4b25cbd0453f4576173184f9741eaca61624c82
jayce.zhao <326747337@qq.com>
2024-09-16 23:21:55 +0800

    feat: add rest

---
+++ lua/plugins/pack-git.lua
@@ -0,0 +1,19 @@
+return {
+  -- Treesitter git support
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "git_config", "gitcommit", "git_rebase", "gitignore", "gitattributes" } },
+  },
+
+  {
+    "nvim-cmp",
+    dependencies = {
+      { "petertriho/cmp-git", opts = {} },
+    },
+    ---@module 'cmp'
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "git" })
+    end,
+  },
+}
